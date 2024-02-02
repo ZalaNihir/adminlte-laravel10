dca480d72f98f4b23b15812303232b97d01ee45f
ZalaNihir <zalanihir8866@gmail.com>
2024-02-02 20:34:36 +0530

    removed the old city migrations & models & providers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CountryStateCityTableSeeder::class);
         $admin = Role::create(['name' => 'admin']);
         $user = Role::create(['name' => 'user']);
 
