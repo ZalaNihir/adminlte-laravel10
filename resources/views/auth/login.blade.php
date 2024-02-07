d1cc42a9ea3011d1e2e6ee8a5617c356fc712dea
ZalaNihir <zalanihir8866@gmail.com>
2024-02-07 20:41:21 +0530

    added the socialite buttons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,6 +49,15 @@
                         <!-- /.col -->
                     </div>
                 </form>
+                <div class="social-auth-links text-center mb-3">
+                    <p>- OR -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+                    </a>
+                </div>
                 <p class="mb-1">
                     <a href="{{ route('password.request') }}">I forgot my password</a>
                 </p>
