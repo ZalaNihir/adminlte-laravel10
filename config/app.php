6b69d372b0d99ee9783be1ded11abca6b0e7d99e
Nihir Zala <zalanihir8866@gmail.com>
2024-02-28 14:31:50 +0530

    update the package depths

--- config/app.php
+++ config/app.php
@@ -169,7 +169,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        \Nihir\CountryStateCity\CountryStateCityServiceProvider::class,
     ])->toArray(),
 
     /*
