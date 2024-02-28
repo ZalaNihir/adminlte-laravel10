5dc7b42c5ef8fa825da8f4e5ddeb71b8e8fd6077
Nihir Zala <zalanihir8866@gmail.com>
2024-02-28 17:41:47 +0530

    fix the seeder

--- database/factories/CollectionFactory.php
+++ database/factories/CollectionFactory.php
@@ -21,9 +21,9 @@ class CollectionFactory extends Factory
         return [
             'name' => $this->faker->word,
             'slug' => $this->faker->slug,
-            'image' => 'path/to/image.jpg', // You can replace this with a path to a real image
-            'pdf' => 'path/to/pdf.pdf', // You can replace this with a path to a real PDF file
-            'category_id' => $this->faker->numberBetween(1, 10), // Assuming you have 10 categories
+            'image' => 'admin/dist/img/avatar.png',
+            'pdf' => 'admin/pdf/dummy.pdf',
+            'category_id' => $this->faker->numberBetween(1, 10),
         ];
     }
 }
