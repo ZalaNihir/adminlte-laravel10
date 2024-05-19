016b10719f0f338d1281cc6a7de968ec622a9c40
ZalaNihir <devnihirzala@gmail.com>
2024-05-19 11:36:17 +0530

    update the image service code

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -31,4 +31,31 @@ class ImageService
 
         return $filename;
     }
+    public function convert($image, $uniqueSlug, $type, $path, $extension)
+    {
+        // Check if image is valid
+        $compressedImage = imagecreatefromstring(file_get_contents($image->getRealPath()));
+        if (!$compressedImage) {
+            // Handle the error or return false
+            return false;
+        }
+        // Set default extension
+        if (!$extension) {
+            $extension = 'webp';
+        }
+        $filename = $uniqueSlug . '.' . $extension;
+        // Determine save path based on the type
+        $savePath = public_path($path . '/' . $type . '/' . $filename);
+        // Ensure directory exists
+        $directory = dirname($savePath);
+        if (!file_exists($directory)) {
+            if (!mkdir($directory, 0755, true)) {
+                // Handle directory creation failure
+                return false;
+            }
+        }
+        imagewebp($compressedImage, $savePath, 100); // Adjust quality as needed
+        imagedestroy($compressedImage);
+        return $filename;
+    }
 }
